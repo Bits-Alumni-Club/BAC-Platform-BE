dbd8a678f4fbd856b7eddcf29cddcb3e6ad9ca9c
John jonas <jonas.humenu@gmail.com>
2021-07-01 15:26:53 +0100

    initial commit

---
+++ BAC_Platform/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BAC_Platform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BAC_Platform.settings')
+
+application = get_asgi_application()
---
+++ README.md
@@ -0,0 +1,7 @@
+# BAC-Platform V1
+
+## User Type requirement
+
+## Security
+
+## terminology
