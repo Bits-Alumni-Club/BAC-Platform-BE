aeded1ff4681f919934a061c5e439d2741cebef1
John jonas <jonas.humenu@gmail.com>
2021-07-07 17:07:17 +0100

    Added user model

--- accounts/models.py
+++ accounts/models.py
@@ -90,3 +90,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=CustomUser)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Event(models.Model):
+    pass
