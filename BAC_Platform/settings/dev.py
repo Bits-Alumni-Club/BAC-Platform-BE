7fe1d2d886db3a049f1f0de1346363b853196fe5
John jonas <jonas.humenu@gmail.com>
2021-07-13 15:38:23 +0100

    heroku deploy

--- BAC_Platform/settings/dev.py
+++ BAC_Platform/settings/dev.py
@@ -5,3 +5,5 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['0.0.0.0', 'bitsalumniclub-api.herokuapp.com', '127.0.0.1']
+
+# adding few changes here 
