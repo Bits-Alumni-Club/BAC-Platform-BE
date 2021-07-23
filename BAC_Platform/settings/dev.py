eb85307585eccdd984c04e31d80f8e419d68120a
John jonas <jonas.humenu@gmail.com>
2021-07-23 12:55:33 +0100

    fixes

--- BAC_Platform/settings/dev.py
+++ BAC_Platform/settings/dev.py
@@ -4,12 +4,11 @@ from .base import *
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-<<<<<<< HEAD
+
 ALLOWED_HOSTS = ['*']
 
 #update here
-=======
+
 ALLOWED_HOSTS = ['0.0.0.0', 'bitsalumniclub-api.herokuapp.com', '127.0.0.1']
 
-# adding few changes here 
->>>>>>> 0241d8008a173ba974ccc9aca0714d3229e31873
+# adding few changes here
