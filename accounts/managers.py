246ad175e38308cf9c2b03bb7abf174eaeb9e311
John jonas <jonas.humenu@gmail.com>
2021-07-23 13:05:49 +0100

    fixes

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,28 +1,23 @@
 from django.contrib.auth.models import BaseUserManager
-<<<<<<< HEAD
-# from . models import CustomUser
-=======
->>>>>>> 0241d8008a173ba974ccc9aca0714d3229e31873
 from django.utils.translation import ugettext as _
 import datetime
 
 
 class CustomUserManager(BaseUserManager):
-<<<<<<< HEAD
+
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email must be set')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         # if password is None:
         #     password = CustomUser.objects.make_random_password(length=10, allowed_chars="abcdefghjkmnpqrstuvwxyz01234567889")
-=======
+
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Email must be set')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         # if not password:
         #     password = make_random_password(length=14, allowed_chars="abcdefghjkmnpqrstuvwxyz01234567889")
->>>>>>> 0241d8008a173ba974ccc9aca0714d3229e31873
         #     user.set_password(password)
         user.set_password(password)
         user.save(using=self._db)
@@ -41,10 +36,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
-<<<<<<< HEAD
         extra_fields.setdefault('is_verified', True)
-=======
->>>>>>> 0241d8008a173ba974ccc9aca0714d3229e31873
         extra_fields.setdefault('year_of_graduation', datetime.date.today().year)
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have a is_staff=True.'))
