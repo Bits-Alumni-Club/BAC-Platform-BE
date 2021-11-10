427cd15449256b46be7c19bbda1a3c3cf74cc8cd
John jonas <jonas.humenu@gmail.com>
2021-11-10 12:55:40 +0100

    code refactor, unit test on accounts

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,9 +1,10 @@
-# import json
+import json
+import datetime
+from django.core import mail
 from rest_framework import status
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from django.core import serializers
-import datetime
 from accounts.models import CustomUser, BitsSchool, Profile
 from faker import Faker
 
@@ -25,149 +26,241 @@ class TestViewSetup(APITestCase):
 
 class TestUserViews(TestViewSetup):
     def test_get_all_users(self):
-        pass
+        bits_school = self.create_bits_school()
+        bits_school_id = BitsSchool.objects.last()
+        faker = Faker()
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": faker.email(),
+                   "phone_number": "081765443", "password": "password", "bits_school": bits_school_id.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+
+        create_resp = self.client.post(reverse('signup'), data=payload, format='json')
+        resp = self.client.get(reverse('users'))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     def test_get_user_details(self):
-        pass
+        faker = Faker()
+        bits_school = self.create_bits_school()
+        user = CustomUser.objects.create_user(first_name=faker.name(), last_name=faker.name(), email=faker.email(),
+                                              phone_number="081765443", password="password", bits_school=bits_school,
+                                              year_of_graduation="2026", country="AF")
+        resp = self.client.get(reverse('user_detail', kwargs={"id": user.id}))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
 
 class TestSignUpViews(TestViewSetup):
     def test_signup_with_invalid_data(self):
-        pass
+        bits_school = self.create_bits_school()
+        # bs = serializers.serialize("json", bits_school)
+        faker = Faker()
+        payload = {"first_name": faker.name(), "last_name": faker.name(),
+                   "phone_number": "081765443", "password": "password", "bits_school": 1,
+                   "year_of_graduation": "2026", "country": "AF"}
+        resp = self.client.post(reverse('signup'), data=payload, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_signup_valid_data(self):
-        pass
+        bits_school = self.create_bits_school()
+        # bs = serializers.serialize("json", bits_school)
+        bits_school_id = BitsSchool.objects.last()
+        faker = Faker()
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": faker.email(),
+                   "phone_number": "081765443", "password": "password", "bits_school": bits_school_id.id,
+                   "year_of_graduation": "2026", "country": "AF"}
 
-    def test_signup_with_unverified_account(self):
-        pass
-
-    def test_signup_with_verified_account(self):
-        pass
+        resp = self.client.post(reverse('signup'), data=payload, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
 
 class TestLogInViews(TestViewSetup):
     def test_login_with_invalid_data(self):
-        pass
+        bits_school = self.create_bits_school()
+        faker = Faker()
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": 1,
+                   "year_of_graduation": "2026", "country": "AF"}
+
+        resp = self.client.post(reverse('login'), data={"email": email, "password": "password1"}, format='json')
+        self.assertEqual(resp.status_code, 401)
 
     def test_login_with_valid_and_unverified_account(self):
-        pass
+        bits_school = self.create_bits_school()
+        faker = Faker()
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": 1,
+                   "year_of_graduation": "2026", "country": "AF"}
+
+        resp = self.client.post(reverse('login'), data={"email": email, "password": password}, format='json')
+        self.assertEqual(resp.status_code, 401)
 
     def test_login_with_valid_and_verified_account(self):
-        pass
+        faker = Faker()
+        bits_school = BitsSchool.objects.create(name="NG1")
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": bits_school.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+        signup = self.client.post(reverse('signup'), data=payload, format='json')
+        user = CustomUser.objects.get(email=email)
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        resp = self.client.post(reverse('login'), data={"email": email, "password": password}, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
 
 class TestForgotPasswordViews(TestViewSetup):
     def test_forgot_password_with_unexisting_email(self):
-        pass
+        resp = self.client.post(reverse('password-reset-email'), data={"email": "dele12@gmail.com"}, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(mail.outbox), 0)
 
     def test_forgot_password_with_existing_email(self):
-        pass
+        faker = Faker()
+        bits_school = BitsSchool.objects.create(name="NG1")
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": bits_school.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+        signup = self.client.post(reverse('signup'), data=payload, format='json')
+        user = CustomUser.objects.get(email=email)
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        resp = self.client.post(reverse('password-reset-email'), data={"email": email}, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(mail.outbox), 1)
 
 
 class TestResetPasswordViews(TestViewSetup):
     def test_password_reset_invalid_link(self):
-        pass
+        faker = Faker()
+        bits_school = BitsSchool.objects.create(name="NG1")
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": bits_school.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+        signup = self.client.post(reverse('signup'), data=payload, format='json')
+        user = CustomUser.objects.get(email=email)
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        resp = self.client.post(reverse('password-reset-email'), data={"email": email}, format='json')
+
+        # Email format body
+        # http://127.0.0.1:8000/password-reset-token-check/MQ/avuurh-afedc57da2210c8891c1f40432747bf5/
+
+        # collect uidb64, token from email body
+        email_body = mail.outbox[0].body.splitlines()
+        link = [i for i in email_body if "/password-reset-token-check/" in i][0]
+        # uidb64, token, empty = link.split("/")[-3:]
+        # email_data = {"uidb64": uidb64, "token": token}
+
+        resp = self.client.patch(reverse('password-reset'),
+                                data={"new_password": "password", "confirm_password": "password", "uidb64": "MA",
+                                      "token": "av1urh-afedc57da2210c8891c1f40432747byet"},
+                                format='json')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_reset_valid_link(self):
-        pass
+        faker = Faker()
+        bits_school = BitsSchool.objects.create(name="NG1")
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": bits_school.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+        signup = self.client.post(reverse('signup'), data=payload, format='json')
+        user = CustomUser.objects.get(email=email)
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        resp = self.client.post(reverse('password-reset-email'), data={"email": email}, format='json')
+
+        # Email format body
+
+        # http://127.0.0.1:8000/password-reset-token-check/MQ/avuurh-afedc57da2210c8891c1f40432747bf5/
+
+        # collect uidb64, token from email body
+
+        email_body = mail.outbox[0].body.splitlines()
+        link = [i for i in email_body if "/password-reset-token-check/" in i][0]
+        uidb64, token, empty = link.split("/")[-3:]
+        email_data = {"uidb64": uidb64, "token": token}
+
+        resp = self.client.patch(reverse('password-reset'), data={"new_password": password,
+                                                                 "confirm_password": password,
+                                                                 "uidb64": email_data['uidb64'],
+                                                                 "token": email_data['token']},
+                                format='json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
     def test_login_after_password_reset_success(self):
-        pass
+        faker = Faker()
+        bits_school = BitsSchool.objects.create(name="NG1")
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": bits_school.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+        signup = self.client.post(reverse('signup'), data=payload, format='json')
+        user = CustomUser.objects.get(email=email)
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        resp = self.client.post(reverse('password-reset-email'), data={"email": email}, format='json')
+
+        # Email format body
+        # http://127.0.0.1:8000/password-reset-token-check/MQ/avuurh-afedc57da2210c8891c1f40432747bf5/
+
+        # collect uidb64, token from email body
+        email_body = mail.outbox[0].body.splitlines()
+        link = [i for i in email_body if "/password-reset-token-check/" in i][0]
+        uidb64, token, empty = link.split("/")[-3:]
+        email_data = {"uidb64": uidb64, "token": token}
+        resp = self.client.patch(reverse('password-reset'), data={"new_password": password,
+                                                                 "confirm_password": password,
+                                                                 "uidb64": email_data['uidb64'],
+                                                                 "token": email_data['token']},
+                                format='json')
+        # Login
+        login_resp = self.client.post(reverse('login'), data={"email": email, "password": password}, format='json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
     def test_login_after_password_reset_fail(self):
-        pass
+        faker = Faker()
+        bits_school = BitsSchool.objects.create(name="NG1")
+        email = faker.email()
+        password = "password"
+        payload = {"first_name": faker.name(), "last_name": faker.name(), "email": email,
+                   "phone_number": "081765443", "password": password, "bits_school": bits_school.id,
+                   "year_of_graduation": "2026", "country": "AF"}
+        signup = self.client.post(reverse('signup'), data=payload, format='json')
+        user = CustomUser.objects.get(email=email)
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        resp = self.client.post(reverse('password-reset-email'), data={"email": email}, format='json')
 
+        # Email format body
+        # http://127.0.0.1:8000/password-reset-token-check/MQ/avuurh-afedc57da2210c8891c1f40432747bf5/
 
-
-#
-#
-# class TestUsers(TestSetup):
-#     def test_get_users(self):
-#         res=self.client.get(self.users_url)
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-#     def test_get_user(self):
-#         res=self.client.get(self.userDetails_url)
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-#
-# class TestSignUp(TestSetup):
-#     def test_signup_with_no_filling_required_field(self):
-#         res=self.client.post(self.signUp_url, data=self.user_data_invalid, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-#
-#     def test_signup_with_valid_details(self):
-#         res=self.client.post(self.signUp_url, data=self.user_data_valid, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-#
-# class TestLogin(TestSetup):
-#     def test_login_with_no_required_field(self):
-#         signup = self.client.post(self.signUp_url, data=self.signUp_data_invalid, format='json')
-#         res=self.client.post(self.logIn_url, data=self.logIn_data_invalid, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-#
-#     def test_login_with_account_not_verified(self):
-#         signup = self.client.post(self.signUp_url, data=self.user_data_valid)
-#         verify = CustomUser.objects.filter(email=signup.data['email'])
-#         cred = {"email": signup.data['email'], "password": signup.data['password']}
-#         res=self.client.post(self.logIn_url, data=cred, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-#
-#     def test_login_with_verified_account(self):
-#         signup = self.client.post(self.signUp_url, data=self.user_data_valid)
-#         verify = CustomUser.objects.filter(email=signup.data['email'])
-#         verify.is_verified = True
-#         verify.is_active = True
-#         verify.save()
-#         cred = {"email": signup.data['email'], "password": signup.data['password']}
-#         res=self.client.post(self.logIn_url, data=cred, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-#     def test_login_with_no_required_field(self):
-#         signup = self.client.post(self.signUp_url, data=self.user_data_invalid)
-#         cred = {"email": signup.data['email'], "password": signup.data['password']}
-#         res=self.client.post(self.logIn_url, data=cred, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-#
-# class TestPasswordResetEmail(TestSetup):
-#     def test_forgot_password_with_no_email(self):
-#         res=self.client.post(self.forgotPassword_url)
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-#
-#     def test_forgot_password_with_no_email(self):
-#         signup = self.client.post(self.signUp_url, data=self.user_data_invalid)
-#         cred = {"email": signup.data['email']}
-#         res=self.client.post(self.forgotPassword_url, data=cred, format='json')
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-#
-# class TestResetPassword(TestSetup):
-#     def test_reset_password_with_no_password(self):
-#         signup = self.client.post(self.signUp_url, data=self.user_data_invalid)
-#         cred = {"email": signup.data['email']}
-#         forgot_password=self.client.post(self.forgotPassword_url, data=cred, format='json')
-#
-#     #     res = self.client.post(self.setNewPassword_url)
-#     #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-#     #
-#     # def test_forgot_password_with_no_email(self):
-#     #     signup = self.client.post(self.signUp_url, data=self.user_data_invalid)
-#     #     cred = {"email": signup.data['email']}
-#     #     res = self.client.post(self.forgotPassword_url, data=cred, format='json')
-#     #     self.assertEqual(res.status_code, status.HTTP_200_OK)
-#
-# # class UserTest(APITestCase):
-# #     def setUp(self):
-# #         self.user1 = CustomUser.objects.create(first_name="jonas", last_name="john", email="jonasjohn@gmail.com",
-# #                                                phone_number="081765443", password="password", bits_school="NGLG",
-# #                                                year_of_graduation="2022", country="AF")
-# #
-# #         self.user2 = CustomUser.objects.create(first_name="david", last_name="alex", email="davidalex@gmail.com",
-# #                                                phone_number="0908366454", password="password", bits_school="NGLG",
-# #                                                year_of_graduation="2022", country="AF")
-# #
-# #         self.login_url = reverse('login')
-#
-#
+        # collect uidb64, token from email body
+        email_body = mail.outbox[0].body.splitlines()
+        link = [i for i in email_body if "/password-reset-token-check/" in i][0]
+        uidb64, token, empty = link.split("/")[-3:]
+        email_data = {"uidb64": uidb64, "token": token}
+        resp = self.client.patch(reverse('password-reset'), data={"new_password": "newpassword",
+                                                                  "confirm_password": "newpassword",
+                                                                  "uidb64": "QU",
+                                                                  "token": email_data['token']},
+                                 format='json')
+        # Login
+        login_resp = self.client.post(reverse('login'), data={"email": email, "password": "newpassword"}, format='json')
+        self.assertEqual(login_resp.status_code, status.HTTP_401_UNAUTHORIZED)
